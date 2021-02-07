14890d49097bf0fb7b0f8ae452ff3bffdc33c3ad
Priscilla Konadu <priscillakonadu@priscillas-air.home>
2021-02-07 20:00:39 +0000

    Added code to demonstrate the use od string operators.

---
+++ basics/input/string_operators.py
@@ -0,0 +1,18 @@
+# Read bot data
+print("Please enter the number of lives.")
+lives = int(input())
+print("Please enter energy level.")
+energy = int(input())
+print("Please enter shield level.")
+shield = int(input())
+
+
+lives = int(input("Please enter the number of lives."))
+
+energy = int(input("Please enter energy level."))
+
+shield = int(input("Please enter shield level."))
+
+print(f"lives:{'♥'* lives} ")
+print(f"energy:{'♦'* energy} ")
+print(f"shield:{'♦'* shield} ")
