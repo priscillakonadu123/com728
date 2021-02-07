5b92e9b6aa700edb151c8dac353fea78b910ff6b
Priscilla Konadu <priscillakonadu@priscillas-air.home>
2021-02-07 23:30:27 +0000

    Created a basic input/output program.

---
+++ basics/input/review.py
@@ -0,0 +1,21 @@
+# Create data for questionnaire
+print ("Hello, what is your name please?")
+name = input()
+print(f"Hello, {name}. Nice to meet you")
+print ("How old are you?")
+age = int(input())
+print(f"Oh, wow. {age} old and looking sooo nice.")
+print ("I am Beep. I would like to take you as a friend")
+
+eye = input("Please enter eye character!")
+print(f"""
+
+\t\t\t\t###########
+\t\t\t\t#  {eye} {eye}     #
+\t\t\t\t#     -    #
+\t\t\t\t#  ---      #
+\t\t\t\t###########
+
+
+
+""")
