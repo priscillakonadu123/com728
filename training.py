7cfe619fc49915eccfeea007b423fef66daaf57b
Priscilla Konadu <priscillakonadu@Priscillas-Air.broadband>
2021-02-14 21:40:46 +0000

    Update

---
+++ training.py
@@ -0,0 +1,24 @@
+# assignment (assign the value)
+a=201
+b=200
+c=50
+d=50
+#== means equal to..
+if b == a:
+    print("b = a")
+    if c == d:
+        print("c = d")
+if a < b:
+    print("a < b")
+else:
+    print("uucuischis")
+
+x = 19
+y = "Priscilla is a girl"
+
+if x > 10:
+  print("Above ten,")
+  if y != "Priscilla is a girl":
+    print("Priscilla is a girl")
+  else:
+    print("Priscilla is not a girl")
