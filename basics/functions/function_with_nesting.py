2cca403dafe3dd74e38bd40ee729c9f6d7d7861b
Priscilla Konadu <priscillakonadu@priscillas-air.home>
2021-02-19 11:53:59 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+def identify():
+    print("Please enter a word representing what you see:")
+    observation = input()
+
+    if observation == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+print()
+
+identify()
+identify()
---
+++ basics/functions/simple_function.py
@@ -0,0 +1,21 @@
+
+# doc string
+
+"""
+purpose
+
+@sound any sound is fine
+"""
+
+
+
+def listen():
+
+    print("What sound did i hear?")
+    sound = input()
+
+    print(f"That was a loud {sound}!")
+
+listen()
+listen()
+listen()
