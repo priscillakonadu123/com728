cf82fa608d9466a3aabeb27f0da886459bea72f7
Priscilla Konadu <priscillakonadu@priscillas-air.home>
2021-02-25 20:40:00 +0000

    Added code to demonstrate the use of a user-defined function with a loop.

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,14 @@
+# BEEP AND BOP CROSSING THE BRIDGE
+
+def cross_brigde(steps):
+
+    for step in range(steps):
+        print("Crossed step.")
+
+    if steps > 5:
+        print("The bridge is collapsing!")
+    else:
+        print("we must keep going!")
+
+cross_brigde(3)
+cross_brigde(6)
