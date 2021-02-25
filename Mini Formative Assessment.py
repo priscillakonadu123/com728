bdd4addf8cb77a459f8aec4ca315098007acdcc0
Priscilla Konadu <priscillakonadu@priscillas-air.home>
2021-02-25 20:14:13 +0000

    finished

--- Mini Formative Assessment.py
+++ Mini Formative Assessment.py
@@ -1,3 +1,31 @@
+FUNCTION: COMMENCE
+
+def commence():
+
+    print("The Avengers Initiative")
+    print("Please enter the number of days:")
+    days = input()
+
+    print(f"The Avengers Initiative will commence in {days} days.")
+
+print()
+commence()
+
+
+FUNCTION: LEAD
+
+def lead(leader):
+    if leader == "Tony Stark":
+        print("The Avengers are ready!")
+    else:
+        print("We need a better leader.")
+
+lead("Hulk")
+
+lead("Tony Stark")
+
+FUNCTION: ASSEMBLE
+
 def assemble(num_avengers):
     print("Assembling Avengers...")
     for count in range(0,num_avengers,1):
