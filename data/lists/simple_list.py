50f2c1e2790c5fbc7edee10327cf930d5e068494
priscillakonadu <4konap05@solent.ac.uk>
2021-03-05 10:16:07 +0000

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,9 @@
+def directions():
+    directions = ["Move Forward", "Move Backward", "Turn left", "Turn Right"]
+    print(directions[0:4])
+    return directions
+
+def run():
+    directions()
+
+run()
