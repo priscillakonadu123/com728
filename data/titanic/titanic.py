fce5cb349937376394e0dd515890f7f00a96a5b1
priscillakonadu <4konap05@solent.ac.uk>
2021-03-11 00:59:30 +0000

    Added code to display a menu of options.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -18,5 +18,23 @@ def run():
     num_records = len(records)
     print(f"Successfully loaded {num_records} records.")
 
+    selected_option = display_menu()
+    print(f"You have selection option: {selected_option}")
+
+
+def display_menu():
+    print( """
+  Please select one of the following options:
+  [1] Display the names of all passengers
+  [2] Display the number of passengers that survived
+  [3] Display the number of passengers per gender
+  [4] Display the number of passengers per age group
+ 
+
+  """)
+
+    selected_response = int(input())
+    return selected_response
+
 if __name__ == "__main__":
     run()
