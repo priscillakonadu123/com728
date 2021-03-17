6954dd46fab3a918209f7e4a3d78efc20eee8eb2
priscillakonadu <4konap05@solent.ac.uk>
2021-03-17 15:20:24 +0000

    Added code to process the Olympics data..

--- data/olympics/data/process.py
+++ data/olympics/data/process.py
@@ -4,11 +4,43 @@ COL_MEDAL = 14
 COL_TEAM = 6
 COL_YEAR = 9
 
-def list_year(data):
+
+def list_years(data):
     tui.started("Listing years")
     years = set()
     for record in data:
         year = record[COL_YEAR]
         years.add(year)
     tui.display_years(years)
+    tui.completed()
+
+
+def tally_medals(data):
+    tui.started("Tallying medals")
+    medal_tally = {"Gold": 0, "Silver": 0, "Bronze": 0}
+    for record in data:
+        medal = record[COL_MEDAL]
+        if medal in ("Gold", "Silver", "Bronze"):
+            medal_tally[medal] += 1
+    tui.display_medal_tally(medal_tally)
+    tui.completed()
+
+
+def tally_team_medals(data):
+    tui.started("Tallying medals for each team")
+    medal_tally = {}
+    for record in data:
+        team = record[COL_TEAM]
+        medal = record[COL_MEDAL]
+
+        if medal not in ("Gold", "Silver", "Bronze"):
+            continue
+
+        if team in medal_tally:
+            medal_tally[team][medal] += 1
+        else:
+            medal_tally[team] = {"Gold": 0, "Silver": 0, "Bronze": 0}
+            medal_tally[team][medal] += 1
+
+    tui.display_team_medal_tally(medal_tally)
     tui.completed()
