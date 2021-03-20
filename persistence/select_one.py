3d4967b7120024db1416081048e3f360ad36fe78
priscillakonadu <4konap05@solent.ac.uk>
2021-03-20 22:28:07 +0000

    Added code to retrieve a single record.

--- persistence/select_one.py
+++ persistence/select_one.py
@@ -5,11 +5,13 @@ import sqlite3
 def retrieve_bot():
     db = sqlite3.connect("Database.db")
     cursor = db.cursor()
-    sql = "SELECT * FROM bots"
+    sql = "SELECT * FROM bots WHERE id=1"
 
     cursor.execute(sql)
 
+#fetch one, all, many with size
     single_row = cursor.fetchall()
+    #record[] records say th 1st or 2nd record in thw list
     print(single_row)
 
     db.close()
