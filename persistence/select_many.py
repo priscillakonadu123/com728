047bf05031a88208e9436cf164f8e4efbe6a9881
priscillakonadu <4konap05@solent.ac.uk>
2021-03-21 00:48:21 +0000

    Added code to retrieve many records.

--- persistence/select_many.py
+++ persistence/select_many.py
@@ -3,19 +3,26 @@ import sqlite3
 def retrieve_bots():
     db = sqlite3.connect("Database.db")
     cursor = db.cursor()
-    sql = "SELECT * FROM bots WHERE id=1"
+    sql = "SELECT * FROM bots;"
 
     cursor.execute(sql)
 
     single_row = cursor.fetchone()
     many_row = cursor.fetchall()
 
+    db.close()
+
+
+
     print("Single bot in the system:")
     print(single_row)
-    print("All bots in the system:")
-    print(many_row)
+    print()
+
+    for record in many_row:
+        print(record)
 
     db.close()
+
 def run():
     retrieve_bots()
 
