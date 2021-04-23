4bfa74ca49f6440b71953f4385778f690a977f64
priscillakonadu <4konap05@solent.ac.uk>
2021-04-23 12:23:08 +0200

    Added code to display multiple plots.

---
+++ visual/plots/squares.py
@@ -0,0 +1,30 @@
+# display the path Beep and Bop are taking through Robo city
+import matplotlib.pyplot as plt
+
+
+def small():
+    x = [3, 3, 4, 4, 3]
+    y = [3, 4, 4, 3, 3]
+    plt.plot(x, y, 'r:o')
+
+
+def medium():
+    x = [2, 2, 5, 5, 2]
+    y = [2, 5, 5, 2, 2]
+    plt.plot(x, y, 'g--s')
+
+
+def large():
+    x = [1, 1, 6, 6, 1]
+    y = [1, 6, 6, 1, 1]
+    plt.plot(x, y, 'b-p')
+
+
+def run():
+    small()
+    medium()
+    large()
+    plt.show()
+
+
+run()
