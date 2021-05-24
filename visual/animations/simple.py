ec42d9fb444f0bb066610d94fb634d4b156271ec
priscillakonadu <4konap05@solent.ac.uk>
2021-05-24 15:15:07 +0100

    Added code to display a simple animation.

---
+++ visual/animations/simple.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+    global ax
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.plot(range(frame), range(frame), 'ro')
+    
+
+
+def run():
+    global fig
+    simple_animation = animation.FuncAnimation(fig,
+                                               animate,
+                                               frames=10,
+                                               interval=1000)
+    plt.show()
+
+run()
